a4609e269a39199fc4bd50a637d95e8541b0ab03
mayalmog <mayalmog@gmail.com>
2022-08-07 20:09:37 +0300

    favicon, cleaning

--- src/cmps/home-page-tabs.jsx
+++ src/cmps/home-page-tabs.jsx
@@ -3,13 +3,10 @@ import React, { useState } from "react";
 export const HomePageTabs = () => {
   const [activeTab, setActiveTab] = useState("tab1");
 
-  //  Functions to handle Tab Switching
   const handleTab1 = () => {
-    // update the state to tab1
     setActiveTab("tab1");
   };
   const handleTab2 = () => {
-    // update the state to tab2
     setActiveTab("tab2");
   };
 
